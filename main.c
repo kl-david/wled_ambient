1383d3ecbd3eaed7da0fdae446a1466c6ce4a478
Kilian David <kilian.david@tum.de>
2024-08-06 10:45:44 +0200

    adds gamma correction table

--- main.c
+++ main.c
@@ -11,6 +11,24 @@
 #define URL "leds.local/json/state"
 #define GAMMA_CORRECTION 1.8
 
+const char gamma8[] = {
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
+    1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
+    2,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,  5,  5,  5,
+    5,  6,  6,  6,  6,  7,  7,  7,  7,  8,  8,  8,  9,  9,  9, 10,
+    10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
+    17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
+    25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
+    37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
+    51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
+    69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
+    90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114,
+    115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
+    144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
+    177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
+    215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255
+};
 typedef struct
 {
     unsigned char r, g, b;
@@ -34,10 +52,9 @@ void calculate_average_rgb(Display *display, Window window, int width, int heigh
     total_rgb.b = 0;
 
     unsigned long pixel;
-    int x, y;
 
-    for (y = 0; y < scaled_height; y++) {
-        for (x = 0; x < scaled_width; x++) {
+    for (int y = 0; y < scaled_height; y++) {
+        for (int x = 0; x < scaled_width; x++) {
             pixel = XGetPixel(image, x*SCALE_DOWN_FACTOR, y*SCALE_DOWN_FACTOR);
             total_rgb.r += (pixel & image->red_mask) >> 16;
             total_rgb.g += (pixel & image->green_mask) >> 8;
@@ -65,6 +82,15 @@ RGB apply_gamma_correction(RGB input) {
     corrected.g = (unsigned char)(pow(input.g / 255.0, GAMMA_CORRECTION) * 255.0);
     corrected.b = (unsigned char)(pow(input.b / 255.0, GAMMA_CORRECTION) * 255.0);
     return corrected;
+}
+
+RGB static_apply_gamma_correction(RGB input) {
+    RGB ret;
+    ret.r = gamma8[input.r];
+    ret.g = gamma8[input.g];
+    ret.b = gamma8[input.b];
+
+    return ret;
 }
 
 void post_rgb(RGB rgb){
@@ -120,8 +146,8 @@ int main() {
         height = attributes.height;
 
         calculate_average_rgb(display, focused_window, width, height, &rgb);
-        rgb = apply_gamma_correction(rgb);
-        printf("Average R: %u, Average G: %u, Average B: %u\n", rgb.r, rgb.g, rgb.b);
+        rgb = static_apply_gamma_correction(rgb);
+        //printf("Average R: %u, Average G: %u, Average B: %u\n", rgb.r, rgb.g, rgb.b);
         post_rgb(rgb);
         nanosleep(&sleep_time, NULL);
     }
